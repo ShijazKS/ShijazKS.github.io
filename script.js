49ac65865a7ce683d6feec8f0c8da044831700d5
ShijazKS <hafizshijaz@gmail.com>
2025-01-07 00:04:53 +0530

    v2

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-var typingDiv = document.getElementById("typing-animation");
 
-if (!typingDiv) {
-  typingDiv = document.createElement("div");
-  typingDiv.id = "typing-animation";
-  document.body.appendChild(typingDiv);
+// cursor
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+    "#a8e6cf",
+    "#9de2c9",
+    "#91dec3",
+    "#86dabc",
+    "#7ad6b6",
+    "#6fd2b0",
+    "#63ceaa",
+    "#58caa4",
+    "#4dc69e",
+    "#42c297",
+    "#37be91",
+    "#2cba8b",
+    "#21b685",
+    "#16b27f",
+    "#0bad79",
+    "#00a973",
+    "#009f68",
+    "#00945e",
+    "#008a54",
+    "#007f4a",
+    "#007540",
+    "#006b36",
+    "#00612c"
+  ];
+
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
 }
 
-// Create a span element for typing animation
-var typingSpan = document.createElement("span");
-typingDiv.appendChild(typingSpan);
-
-function typeWriter(text, i) {
-  if (i < text.length) {
-    typingSpan.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(function () {
-      typeWriter(text, i);
-    }, 50);
-  } else {
-    setTimeout(function () {
-      eraseText(text.length);
-    }, 1000);
-  }
-}
-
-function eraseText(length) {
-  var text = typingSpan.innerHTML;
-  if (text.length > 0) {
-    text = text.substring(0, text.length - 1);
-    typingSpan.innerHTML = text;
-    setTimeout(function () {
-      eraseText(length);
-    }, 30);
-  } else {
-    setTimeout(function () {
-      typeWriter("I am using Archlinux BTW :)", 0);
-    }, 1000);
-  }
-}
-
-typeWriter("I am using Archlinux BTW :)", 0);
-
+animateCircles();
 
 
